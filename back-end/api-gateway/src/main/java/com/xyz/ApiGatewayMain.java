44b2d668590786e1b2da250ab3c22c342b519707
yanzhe-Xiao <2397903439@qq.com>
2025-09-01 20:38:55 +0800

    api-gateway

---
+++ back-end/api-gateway/src/main/java/com/xyz/ApiGatewayMain.java
@@ -0,0 +1,21 @@
+package com.xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Package Name: com.xyz </p>
+ * <p>Description:  </p>
+ * <p>Create Time: 2025/9/1 </p>
+ *
+ * @author <a href="https://github.com/yanzhe-xiao">YANZHE XIAO</a>
+ * @version 1.0
+ * @since
+ */
+
+@SpringBootApplication
+public class ApiGatewayMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayMain.class,args);
+    }
+}
---
+++ back-end/api-gateway/src/main/java/com/xyz/config/GatewaySecurityConfig.java
@@ -0,0 +1,36 @@
+package com.xyz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * <p>Package Name: com.xyz.config </p>
+ * <p>Description:  </p>
+ * <p>Create Time: 2025/9/1 </p>
+ *
+ * @author <a href="https://github.com/yanzhe-xiao">YANZHE XIAO</a>
+ * @version 1.0
+ * @since 21
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex
+                        .pathMatchers("/auth/**", "/actuator/health").permitAll()
+                        .anyExchange().authenticated()
+                )
+
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
